232a7cabea967f8f2cd8d8df40c390eb12c533ef
gaeldariog-make <gaeldariog@gmail.com>
2025-12-02 12:03:21 -0600

    Update pru.py

--- pru.py
+++ pru.py
@@ -1,29 +1,22 @@
 import streamlit as st
 import pandas as pd
 import time
-'iniciando...'
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteracion {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-"listo"
-
-add_selectbox = st.sidebar.selectbox(
-    'selecciona opcion',
-    ('Email', 'Home phone', 'Mobile phone')
-)
-"escogiste", add_selectbox
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'valores',
-    0.0, 100.0, (25.0, 75.0)
+i=1
+while i=1:
+  'iniciando...'
+  
+  # Add a placeholder
+  latest_iteration = st.empty()
+  bar = st.progress(0)
+  
+  for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteracion {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+  
+  "listo"
+  i+=1
 )
 st.write("mis datos:")
 st.write(pd.DataFrame({
@@ -31,6 +24,22 @@ st.write(pd.DataFrame({
     'second column': [10, 20, 30, 40]
 
 }))
+opcion = st.sidebar.selectbox(
+    'quieres ingresar un request?',
+    ('si', 'no')
+)
+if opcion=='si':
+  add_selectbox = st.sidebar.selectbox(
+      'selecciona opcion',
+      ('1', '2', '3','4')
+  )
+  "escogiste", add_selectbox
+  pd.('add_selectbox')
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'valores',
+    0.0, 100.0, (25.0, 75.0)
+
 
 
 
