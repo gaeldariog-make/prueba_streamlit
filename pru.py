db44b4950a0248671d446336efd79da51e51042a
gaeldariog-make <gaeldariog@gmail.com>
2025-12-02 11:38:08 -0600

    Update pru.py

--- pru.py
+++ pru.py
@@ -1,8 +1,32 @@
 import streamlit as st
 import pandas as pd
+'iniciando...'
 
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteracion {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+"listo"
+
+add_selectbox = st.sidebar.selectbox(
+    'selecciona opcion',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+"escogiste", add_selectbox
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'valores',
+    0.0, 100.0, (25.0, 75.0)
+)
 st.write("mis datos:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
+
 }))
