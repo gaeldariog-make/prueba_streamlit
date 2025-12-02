a9a5a5d6fb71c1ce9c0b6114d55882cb64bc72c2
gaeldariog-make <gaeldariog@gmail.com>
2025-12-02 12:19:47 -0600

    Update pru.py

--- pru.py
+++ pru.py
@@ -2,22 +2,22 @@ import streamlit as st
 import pandas as pd
 import time
 
-while True:
-  if i==1:
-    break
-  'iniciando...'
+
   
-  # Add a placeholder
-  latest_iteration = st.empty()
-  bar = st.progress(0)
-  
-  for i in range(100):
-    # Update the progress bar with each iteration.
-    latest_iteration.text(f'{i+1}%')
-    bar.progress(i + 1)
-    time.sleep(0.01)
-  
-  "listo"
+    
+'iniciando...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'{i+1}%')
+  bar.progress(i + 1)
+  time.sleep(0.01)
+
+"listo"
   
 df=pd.DataFrame({
     'first column': [1, 2, 3, 4],
@@ -40,11 +40,12 @@ if opcion=='si':
     df['first column']
   if add_selectbox=='2':
     df['second column']
-i=1
+
 # Add a slider to the sidebar:
 #add_slider = st.sidebar.slider(
 #    'valores',
 #    0.0, 100.0, (25.0, 75.0))
+
 
 
 
